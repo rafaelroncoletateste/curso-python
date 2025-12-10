1367735e5119eff80bf3c5aa6cafb19128198190
Rafael <rafaelnh089@hotmail.com>
2025-12-10 20:15:12 -0300

    operadores aritméticos

---
+++ aula9.py
@@ -0,0 +1,20 @@
+adicao = 5 + 5
+print(adicao)
+
+subtracao = 5 - 5
+print(subtracao)
+
+multiplicacao = 5 * 5
+print(multiplicacao)
+
+divisao = 5 / 5
+print(divisao)
+
+divisao_inteira = 10 // 2.2
+print(divisao_inteira)
+
+exponenciacao = 2 ** 6
+print(exponenciacao)
+
+modulo = 22 % 3
+print(modulo)
