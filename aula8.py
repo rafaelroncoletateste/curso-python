f7aee3f2c7033a37b0303b65974506ce9748b559
Rafael <rafaelnh089@hotmail.com>
2025-12-10 20:04:28 -0300

    variáveis e tipos de dados

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Rafael'
+sobrenome = 'Roncoleta'
+idade = 20
+ano_nascimento = 2005
+maior_de_idade = 2025 - ano_nascimento >= 18
+altura_metros = 1.8
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade:", maior_de_idade)
+print("Altura em metros:", altura_metros)
