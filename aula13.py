8acfacede91cffa5e81ea8e320fed4b0af279280
Rafael <rafaelnh089@hotmail.com>
2025-12-11 19:13:42 -0300

    introdução a f-strings

---
+++ aula13.py
@@ -0,0 +1,11 @@
+nome = 'Rafael'
+altura = 1.8
+peso = 70
+imc = peso / (altura ** 2)
+
+linha_1 = f"{nome} tem {altura:.2f} de altura"
+linha_2 = f'pesa {peso} quilos e seu imc é'
+
+print(linha_1)
+print(linha_2)
+print(imc) 
