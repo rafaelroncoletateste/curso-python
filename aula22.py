ba54029263c6d2ea2686af6c7469be2bfbfe9f16
Rafael <rafaelnh089@hotmail.com>
2025-12-15 19:52:55 -0300

    operador lógico "not"

---
+++ aula22.py
@@ -0,0 +1,12 @@
+"""
+not -> Usado para inverter expressões
+"""
+
+# senha = input("Digite sua senha: ")
+
+# if senha == '123':
+#     print("Entrou")
+# else:
+#     print("Senha incorreta")
+
+print(not 0)
