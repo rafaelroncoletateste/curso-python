f3c703dc780c2ef271ae2f27981a0c4a3499d81b
Rafael <rafaelnh089@hotmail.com>
2025-12-15 18:54:28 -0300

    condicionais

---
+++ aula16.py
@@ -0,0 +1,12 @@
+# if / elif      / else
+# se / se não se / se não
+
+entrada = input("Você deseja 'entrar' ou 'sair'? ")
+
+if entrada == 'entrar':
+    print("Você entrou")
+elif entrada == 'sair':
+    print("Você saiu")
+else:
+    print("Você não tomou uma decisão")
+
