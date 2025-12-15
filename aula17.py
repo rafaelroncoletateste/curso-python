a7a0bbf6a389ed95a71234c29503aa8be4017b99
Rafael <rafaelnh089@hotmail.com>
2025-12-15 19:14:14 -0300

    entendendo o fluxo do interpretador (em condicionais)

---
+++ aula17.py
@@ -0,0 +1,15 @@
+condicao = True
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao:
+    print('Código do bloco IF')
+elif condicao2:
+    print('Código do ELIF')
+elif condicao3:
+    print('Código do ELIF')
+elif condicao4:
+    print('Código do ELIF')
+else:
+    print('Passou por todos!')
