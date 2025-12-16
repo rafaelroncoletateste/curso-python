e8a4eaaf32ef558764c9e08283ff43985e73e877
Rafael <rafaelnh089@hotmail.com>
2025-12-16 19:28:47 -0300

    f-strings

---
+++ aula25.py
@@ -0,0 +1,10 @@
+"""
+> - Esquerda
+< - Direita
+^ - Centro
+"""
+
+variavel = 'ABC'
+print(f'{variavel:4>10}') # Vai preencer a esquerda da variável com 10 números 4
+
+print(f'{1223.6784775674567:,.1f}')
