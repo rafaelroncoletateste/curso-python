d30eb2df287ed4bea88cad0f83376bbae8386acf
Rafael <rafaelnh089@hotmail.com>
2025-12-17 20:17:02 -0300

    flasg, is, is not, none

--- aula31.py
+++ aula31.py
@@ -1,2 +1,19 @@
-v1 = 'a'
-print(id(v1))
+"""
+Flag -> marcar um local
+None -> sem valor
+is | is not -> é ou não é
+id -> identidade
+"""
+
+# v1 = 'a'
+# print(id(v1))
+
+condicao = True
+passou_no_if = None
+
+if condicao:
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+print(passou_no_if is None)
