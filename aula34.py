e975930d0d641fd9e080599c734d8355cdf0a005
Rafael <rafaelnh089@hotmail.com>
2025-12-19 20:03:42 -0300

    while

---
+++ aula34.py
@@ -0,0 +1,10 @@
+"""
+while(enquanto) -> Executa uma ação enquanto uma condição for verdadeira
+"""
+
+condicao = True
+
+# Loop infinito, pois a condição nunca se altera
+while condicao:
+    nome = input('Digite o seu nome: ')
+    print(nome)
