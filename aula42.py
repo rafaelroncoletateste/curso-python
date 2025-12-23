4d5ddeff723dcf0ba7ff416cfdec23f236d0d560
Rafael <rafaelnh089@hotmail.com>
2025-12-23 20:06:14 -0300

    iterando strings com while

---
+++ aula42.py
@@ -0,0 +1,11 @@
+frase = 'O python é uma linguagem de programação'
+
+print(frase.count('python')) # Quantas vezes a palavra python aparece na string
+
+i = 0
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    print(letra_atual)
+    # i += 1
