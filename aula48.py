24a8b972b2e415f99f330f3e528dc7f002628dbe
Rafael <rafaelnh089@hotmail.com>
2025-12-28 10:42:03 -0300

    inserindo itens em qualquer índice

--- aula48.py
+++ aula48.py
@@ -1,3 +1,11 @@
+# append -  Adiciona um item no final
+# insert - Adiciona no índice escolhido
+# pop - Remove do final ou do índice escolhido
+# del - Apaga um índice
+# clear - Limpa a lista
+# extend - Estende a lista
+
+
 # string = 'ABCDE'
 
 # # lista = list()
@@ -6,9 +14,15 @@
 # lista = [123, 'Rafael', 1.23, outraLista]
 # print(lista)
 
-listaNumeros = [10, 20, 30 ,40]
-del listaNumeros[3]
-print(listaNumeros)
+# listaNumeros = [10, 20, 30 ,40]
+# del listaNumeros[3]
+# print(listaNumeros)
 
-listaNumeros.append(50) # Adiciona ao final da lista
-listaNumeros.pop() # Remove o último elemento da lista
+# listaNumeros.append(50) # Adiciona ao final da lista
+# listaNumeros.pop() # Remove o último elemento da lista
+
+listaNumeros = [10, 20, 30 ,40]
+ultimoItem = listaNumeros.pop()
+listaNumeros.insert(0, 1200)
+
+print(ultimoItem)
