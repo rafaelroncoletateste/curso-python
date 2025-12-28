a52e991529e87d852ea920aadd3aa8b460a46409
Rafael <rafaelnh089@hotmail.com>
2025-12-28 10:26:47 -0300

    alterando uma lista com índices

--- aula48.py
+++ aula48.py
@@ -1,7 +1,14 @@
-string = 'ABCDE'
+# string = 'ABCDE'
 
-# lista = list()
+# # lista = list()
 
-outraLista = ['a', 'b']
-lista = [123, 'Rafael', 1.23, outraLista]
-print(lista)
+# outraLista = ['a', 'b']
+# lista = [123, 'Rafael', 1.23, outraLista]
+# print(lista)
+
+listaNumeros = [10, 20, 30 ,40]
+del listaNumeros[3]
+print(listaNumeros)
+
+listaNumeros.append(50) # Adiciona ao final da lista
+listaNumeros.pop() # Remove o último elemento da lista
