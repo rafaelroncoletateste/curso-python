d035f1ec1fa593f5ca971960a52aae44ceddb526
Rafael <rafaelnh089@hotmail.com>
2025-12-29 13:08:10 -0300

    Introdução ao empacotamento e desempacotamento

---
+++ aula51.py
@@ -0,0 +1,7 @@
+"""
+Introdução ao desempacotamento + tuples (tuplas)
+"""
+
+# nomes = ['Rafael', 'Gabriel', 'Vitor']
+_, nome2, *_ = ['Rafael', 'Gabriel', 'Vitor'] # Se usa o "_" para variáveis que não seram usadas
+print(_)
